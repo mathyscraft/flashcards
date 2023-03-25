3672ea64a9331d28135ad22e189f28a071e437b3
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-03-25 21:23:10 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,470 @@
+
+var points=0
+var answered=0
+var question=new Array
+
+i=0
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est roi en France au début de la Révolution française ?"
+question[i].reponse="LOUIS XVI"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel est le nom de l'épouse de Louis XVI ?"
+question[i].reponse="MARIE-ANTOINETTE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment (mot au pluriel) appelle-t-on les personnes soumises à l'autorité d'un souverain ?"
+question[i].reponse="SUJETS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Combien (chiffre) y a-t-il d'ordres dans la société d'ordres ?"
+question[i].reponse="3"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel est l'ordre social le plus nombreux avant la Révolution ?"
+question[i].reponse="TIERS ETAT"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qu'est-ce que Louis XVI convoque au début de 1789 ?"
+question[i].reponse="ETATS GENERAUX"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne un changement brusque et important d'une situation ?"
+question[i].reponse="REVOLUTION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on des personnes qui exercent un rôle politique dans un pays ?"
+question[i].reponse="CITOYENS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne l'ensemble des citoyens d'un terrioire ?"
+question[i].reponse="NATION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel événement révolutionnaire se produit le 17 juin 1789 ?"
+question[i].reponse="LE TIERS ETAT SE PROCLAME ASSEMBLEE NATIONALE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour le Tiers Etat se proclame-t-il Assemblée nationale ?"
+question[i].reponse="17 JUIN 1789"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on une monarchie où le roi n'a pas tous les pouvoirs ?"
+question[i].reponse="MONARCHIE LIMITEE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour est prêté le Serment du Jeu de paume ?"
+question[i].reponse="20 JUIN 1789"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom porte le texte juré par les députés du tiers état par lequel ils déclarent ne pas se séparer avant d'avoir donné une constitution à la France ?"
+question[i].reponse="SERMENT DU JEU DE PAUME"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour de 1789 l'Assemblée nationale se déclare-t-elle constituante ?"
+question[i].reponse="9 JUILLET 1789"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour tombe la Bastille ?"
+question[i].reponse="14 JUILLET 1789"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mouvement populaire irrationnel commence à se déclencher le 20 juillet 1789 ?"
+question[i].reponse="GRANDE PEUR"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qu'abolit-on durant la nuit du 4 août 1789 ?"
+question[i].reponse="PRIVILEGES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A quelle date est adoptée la Déclaration des Droits de l'Homme et du Citoyen ?"
+question[i].reponse="26 AOUT 1789"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qu'adopte-t-on le 26 août 1789 ?"
+question[i].reponse="DECLARATION DES DROITS DE L'HOMME ET DU CITOYEN"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Sous quel nom général regroupe-t-on la liberté de la presse, la liberté de se déplacer, la liberté d'expression... ?"
+question[i].reponse="LIBERTES FONDAMENTALES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on le mouvement d'opposition à l'ensemble des transformations provoquées par la Révolution française ?"
+question[i].reponse="CONTRE-REVOLUTION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Dans quel palais s'installe la famille royale à partir du 6 octobre 1789 ?"
+question[i].reponse="TUILERIES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel type de suffrage est pratiqué à l'époque de la monarchie limitée ?"
+question[i].reponse="SUFFRAGE CENSITAIRE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comme appelle-t-on le suffrage quand celui-ci est réservé aux plus riches ?"
+question[i].reponse="SUFFRAGE CENSITAIRE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel texte du 12 juillet 1790 a pour but de transformer la place et le rôle de l'Eglise en France ?"
+question[i].reponse="CONSTITUTION CIVILE DU CLERGE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle cérémonie se tient à Paris le 14 juillet 1790 ?"
+question[i].reponse="FETE DE LA FEDERATION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A quelle date se tient la Fête de la Fédération ?"
+question[i].reponse="14 JUILLET 1790"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois plus année) Louis XVI s'enfuit-il de Paris ?"
+question[i].reponse="JUIN 1791"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle assemblée se réunit pour la première fois le 1er octobre 1791 ?"
+question[i].reponse="ASSEMBLEE LEGISLATIVE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour se réunit pour la première fois l'Assemblée législative ?"
+question[i].reponse="1ER OCTOBRE 1791"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom porte le club où se réunissent à l'époque de la monarchie limitée ceux qui soutiennent le régime ?"
+question[i].reponse="FEUILLANTS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom porte le club où se réunissent à l'époque de la monarchie limitée les révolutionnaires les plus progressistes ?"
+question[i].reponse="JACOBINS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom porte la procédure qui permet à Louis XVI de bloquer une loi pour deux ans ?"
+question[i].reponse="VETO"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois+année) la France entre-t-elle en guerre contre l'Autriche ?"
+question[i].reponse="AVRIL 1792"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour les Parisiens prennent-ils les Tuileries ?"
+question[i].reponse="10 AOUT 1792"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle est la conséquence de la prise des Tuileries en août 1792 ?"
+question[i].reponse="SUSPENSION DE LOUIS XVI"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle est la première assemblée élu au suffrage universel ?"
+question[i].reponse="CONVENTION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle assemblée tient sa première séance le 21 septembre 1792 ?"
+question[i].reponse="CONVENTION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Que décide la Convention le 21 septembre 1792 ?"
+question[i].reponse="ABOLITION DE LA ROYAUTE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle assemblée vote l'abolition de la monarchie en France ?"
+question[i].reponse="CONVENTION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est guillotiné le 21 janvier 1793 ?"
+question[i].reponse="LOUIS XVI"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour Louis XVI est-il guillotiné ?"
+question[i].reponse="21 JANVIER 1793"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel organe politique est créé en France le 6 avril 1793 ?"
+question[i].reponse="COMITE DE SALUT PUBLIC"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année l'Ouest de la France se soulève-t-il contre le gouvernement républicain ?"
+question[i].reponse="1793"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois + année) commence la Terreur ?"
+question[i].reponse="SEPTEMBRE 1793"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour Robespierre est-il arrêté ?"
+question[i].reponse="27 JUILLET 1794"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A quel jour du calendrier correspond le 27 juillet 1794 ?"
+question[i].reponse="9 THERMIDOR"
+question[i].indice="--"
+
+//
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle expression désigne la Convention après la chute des Montagnards ?"
+question[i].reponse="CONVENTION THERMIDORIENNE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nouveau système politique entre en vigueur le 26 octobre 1795 ?"
+question[i].reponse="DIRECTOIRE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Combien (chiffre) y a-t-il de Directeurs sous le Directoire ?"
+question[i].reponse="5"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Combien (chiffre) y a-t-il d'assemblées sous le Directoiree ?"
+question[i].reponse="2"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel général se révèle lors de la campagne d'Italie de 1796-1797 ?"
+question[i].reponse="NAPOLEON BONAPARTE"
+question[i].indice="--"
+
+// 1799
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom donne-t-on au coup d'Etat de Napoléon Bonaparte ?"
+question[i].reponse="18 BRUMAIRE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour commence le coup d'Etat du 18 Brumaire ?"
+question[i].reponse="9 NOVEMBRE 1799"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom porte le système politique succédant au Directoire ?"
+question[i].reponse="CONSULAT"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année commence le Consulat ?"
+question[i].reponse="1799"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A qui doit-on la phrase \"La Révolution est fixée aux principes qui l'ont commencée ; elle est finie \" ?"
+question[i].reponse="NAPOLEON BONAPARTE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année Napoléon Bonaparte obtient-il des Français le titre d'empereur ?"
+question[i].reponse="1804"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel recueil de lois est publié en 1804 ?"
+question[i].reponse="CODE CIVIL"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle retentissante victoire Napoléon Ier remporte-t-il sur les Russes et les Autrichiens le 2 décembre 1805 ?"
+question[i].reponse="AUSTERLITZ"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année l'empire napoléonien est-il à son apogée ?"
+question[i].reponse="1811"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel pays Napoléon Ier attaque-t-il en 1812 dans une campagne qui va se révéler désastreuse ?"
+question[i].reponse="RUSSIE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois+année) Napoléon Ier abdique-t-il pour la première fois ?"
+question[i].reponse="AVRIL 1814"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on la période de l'éphémère retour de Napoléon Ier ?"
+question[i].reponse="CENT JOURS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle bataille marque la fin de la période napoléonienne ?"
+question[i].reponse="WATERLOO"
+question[i].indice="--"
---
+++ index.js
@@ -0,0 +1,110 @@
+const flashcard = document.getElementById("flashcard")
+const flashcardContainer = document.getElementById("flashcard-container")
+const output = document.getElementById("output")
+const answer = document.getElementById("answer")
+const pointSpan = document.getElementById("points")
+const progressBar = document.getElementById("progress-bar")
+const progressStat = document.getElementById("progress-stat")
+let currentQuestion = {}
+const initialQuestionLength = question.length
+getQuestion()
+
+function getQuestion() {
+    answer.hidden = true
+    output.hidden = false
+    flashcard.style.opacity = "0"
+    setTimeout(function() {
+        flashcard.style.left = "50%"
+        flashcard.style.opacity = "1"
+         // Choisir une question aléatoire qui n'a pas encore été posée
+        let unansweredQuestions = question.filter(q => !q.asked);
+        if (unansweredQuestions.length === 0) {
+            question = question.filter(q => !q.correct)
+            for (i in question) {
+                question[i].asked = false
+            }
+            if (question.length === 0) {
+                output.innerHTML = "Bravo, vous avez répondu à toutes les questions !"
+                answer.innerHTML = "Recharchez la page pour rejouer"
+                return;
+            } else if (question.length <= 0.1*initialQuestionLength) {
+                progressBar.style.borderColor = "green"
+            } else if (question.length <= 0.35*initialQuestionLength) {
+                progressBar.style.borderColor = "orange"
+            } else {
+                progressBar.style.borderColor = "red"
+            }
+            unansweredQuestions = question.filter(q => !q.asked);
+            console.log(question)
+        }
+        let randomIndex = Math.floor(Math.random() * unansweredQuestions.length);
+        currentQuestion = unansweredQuestions[randomIndex];
+    
+        // Afficher la question à l'utilisateur
+        output.innerHTML = currentQuestion.texte;
+        answer.innerHTML = currentQuestion.reponse;
+    
+        // Mettre à jour le nombre de questions répondues et marquer la question comme posée
+        answered++;
+        currentQuestion.asked = true;
+        progressBar.style.borderLeftWidth = ((question.length + 1 - unansweredQuestions.length) / question.length) * 300 + "px"
+        progressStat.innerHTML = (question.length + 1 - unansweredQuestions.length) +"/"+ question.length
+    }, 800)
+}
+
+flashcard.addEventListener("click", function() {
+    if (flashcard.style.left == "50%") {
+        answer.hidden = !answer.hidden
+        output.hidden = !output.hidden
+    }
+})
+
+flashcard.addEventListener("mousedown", function() {
+    document.onmousemove = function(e){
+        flashcard.style.left = e.clientX + window.scrollX +"px"
+    }
+})
+flashcardContainer.addEventListener("mouseup", function() {
+    let flashcardX = flashcard.style.left
+    flashcardX = flashcardX.replace("px","")
+    if (flashcardX <= 0.3*window.innerWidth) {
+        console.log("false")
+        currentQuestion.correct = false
+        getQuestion()
+
+    } else if (flashcardX > 0.7*window.innerWidth) {
+        console.log("correct")
+        points++;
+        pointSpan.innerHTML = points;
+        currentQuestion.correct = true
+        getQuestion()
+    } else {
+        setTimeout(function() {
+            flashcard.style.left = "50%"
+        }, 100)
+    }
+    document.onmousemove = function() {return}
+})
+
+flashcard.addEventListener("touchstart", function() {
+    document.ontouchmove = function(e){
+      flashcard.style.left = e.touches[0].clientX + window.pageXOffset + "px";
+    }
+  })
+  
+  flashcardContainer.addEventListener("touchend", function() {
+    let flashcardX = flashcard.style.left.replace("px", "");
+    if (flashcardX <= 0.3*window.innerWidth) {
+      console.log("faux");
+      getQuestion();
+    } else if (flashcardX > 0.7*window.innerWidth) {
+      console.log("vrai");
+      getQuestion();
+    } else {
+      setTimeout(function() {
+        flashcard.style.left = "50%";
+      }, 100);
+    }
+    document.ontouchmove = function() {return;}
+  })
+  
