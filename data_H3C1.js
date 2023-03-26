48c0cd002c4bb879d9c52f2d1af2e5fc6b32025e
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-03-26 20:21:05 +0200

    new chapter

---
+++ data_H3C1.js
@@ -0,0 +1,368 @@
+
+var points=0
+var answered=0
+var question=new Array
+
+i=0
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour est proclamé la Troisième République ?"
+question[i].reponse="4 SEPTEMBRE 1870"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel régime politique français commence le 4 septembre 1870 ?"
+question[i].reponse="TROISIEME REPUBLIQUE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne un système politique qui repose sur la volonté du peuple ?"
+question[i].reponse="DEMOCRATIE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne un système politique dont le chef d'Etat n'est pas un souverain ?"
+question[i].reponse="REPUBLICAIN"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle période de troubles dure du 18 mars au 28 mai 1871 ?"
+question[i].reponse="COMMUNE DE PARIS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année se déroule la Commune de Paris ?"
+question[i].reponse="1871"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle femme est une personnalité marquante de la Commune ?"
+question[i].reponse="LOUISE MICHEL"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel dirigeant ordonne l'intervention de l'armée contre la Commune de Paris ?"
+question[i].reponse="ADOLPHE THIERS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour commence la Commune de Paris ?"
+question[i].reponse="18 MARS 1871"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour se termine la Commune de Paris ?"
+question[i].reponse="28 MAI 1871"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on la dernière semaine de la Commune de Paris ?"
+question[i].reponse="SEMAINE SANGLANTE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est le premier Président de la République de la Troisième République ?"
+question[i].reponse="ADOLPHE THIERS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est le deuxième Président de la République de la Troisième République ?"
+question[i].reponse="PATRICE DE MAC MAHON"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel Président de la République dit qu'il \"garde la place\" en attendant une restauration de la monarchie ?"
+question[i].reponse="PATRICE DE MAC MAHON"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année sont votées les lois constitutionnelles de la Troisième République ?"
+question[i].reponse="1875"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année la crise éclate-t-elle entre Mac Mahon et Les Républicains ?"
+question[i].reponse="1877"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année les Républicains prennent-ils le contrôle complet de la République ?"
+question[i].reponse="1879"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année Patrice de Mac Mahon démissionne-t-il ?"
+question[i].reponse="1879"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année est célébré le premier 14 juillet ?"
+question[i].reponse="1880"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Que célèbre-t-on pour la première fois le 14 juillet 1880 ?"
+question[i].reponse="FETE NATIONALE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année est votée la loi sur la liberté de la presse ?"
+question[i].reponse="1881"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle loi fondamentale est votée en 1881 ?"
+question[i].reponse="LOI SUR LA LIBERTE DE LA PRESSE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle grande figure républicaine ayant proclamé la République le 4 septembre 1870 meurt d'un accident avec une arme à feu fin 1882."
+question[i].reponse="LEON GAMBETTA"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A quel homme politique français doit-on les lois sur l'Ecole de 1881-1882 ?"
+question[i].reponse="JULES FERRY"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année est votée la loi sur les syndicats ?"
+question[i].reponse="1884"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle loi importante est votée en 1884 ?"
+question[i].reponse="LOI AUTORISANT LES SYNDICATS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Que rouvre-t-on à l'occasion de la mort de Victor Hugo ?"
+question[i].reponse="PANTHEON"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A l'occasion de la mort de qui rouvre-t-on le Panthéon ?"
+question[i].reponse="VICTOR HUGO"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année meurt Victor Hugo ?"
+question[i].reponse="1885"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment qualifie-t-on les républicains modérés qui gouvernent la France de 1879 à 1902 ?"
+question[i].reponse="OPPORTUNISTES"
+question[i].indice="--"
+
+
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année la France entre-t-elle dans la Première Guerre mondiale ?"
+question[i].reponse="1914"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel militaire français menace la République par sa grande popularité auprès des nationalistes antirépublicains ?"
+question[i].reponse="GEORGES BOULANGER"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="De qui les Boulangistes sont-ils partisans ?"
+question[i].reponse="GEORGES BOULANGER"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle grande affaire judiciaire dure de 1894 à 1906 et divise la France ?"
+question[i].reponse="AFFAIRE DREYFUS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année commence l'affaire Dreyfus ?"
+question[i].reponse="1894"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel capitaine de l'armée française se trouve accusé d'espionnage au profit de l'Allemagne en 1894 ?"
+question[i].reponse="ALFRED DREYFUS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois+année) Zola publie-t-il son <em>J'accuse</em> ?"
+question[i].reponse="JANVIER 1898"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est l'auteur de l'article <em>J'accuse</em> ?"
+question[i].reponse="EMILE ZOLA"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel est le titre de la lettre ouverte que publie Emile Zola dans l'Aurore en janvier 1898 ?"
+question[i].reponse="J'ACCUSE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle loi le gouvernement de Défense républicaine fait-il voter en 1901 ?"
+question[i].reponse="LOI SUR LES ASSOCIATIONS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année est adoptée la loi sur les associations ?"
+question[i].reponse="1901"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année les Républicains radicaux accèdent-ils à la tête du gouvernement ?"
+question[i].reponse="1902"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour est adoptée la loi de séparation des Eglises et de l'Etat ?"
+question[i].reponse="9 DECEMBRE 1905"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qu'introduit dans l'Etat républicain la loi du 9 décembre 1905 ?"
+question[i].reponse="LAICITE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle loi est votée par les radicaux le 9 décembre 1905 ?"
+question[i].reponse="LOI DE SEPARATION DES EGLISES ET DE L'ETAT"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année Dreyfus est-il définitivement innocenté ?"
+question[i].reponse="1906"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Sur quelle période Georges Clemenceau est-il président du Conseil ? (format de la réponse XXXX-XXXX)"
+question[i].reponse="1906-1909"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on des personnes qui placent l'intérêt de la nation avant même les lois et les principes de la République ?"
+question[i].reponse="NATIONALISTES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel est le personnage avec lequel vous avez conduit un dialogue virtuel ?"
+question[i].reponse="GEORGES CLEMENCEAU"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment qualifie-t-on les républicains les plus à gauche qui dominent le gouvernement à partir de 1902 ?"
+question[i].reponse="RADICAUX"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot (au pluriel) désigne les républicains les plus avancés favorables à la laïcité et à plus d'égalité entre les Français ?"
+question[i].reponse="RADICAUX"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on un système politique comme celui de la Troisième République dans lequel la majorité à l'Assemblée détermine qui doit gouverner ?"
+question[i].reponse="SYSTEME PARLEMENTAIRE"
+question[i].indice="--"
--- navigation.js
+++ navigation.js
@@ -4,6 +4,8 @@ const overlay = document.getElementById("overlay");
 
 const buttonH1C1 = document.getElementById("H1C1");
 const buttonH1C2 = document.getElementById("H1C2");
+const buttonH3C1 = document.getElementById("H3C1");
+
 
 hamburger.addEventListener("click", function() {
     menu.hidden = !menu.hidden
@@ -24,6 +26,17 @@ buttonH1C1.addEventListener("click", function() {
 buttonH1C2.addEventListener("click", function() {
     let dataFile = document.createElement("script")
     dataFile.setAttribute("src", "data_H1C2.js")
+    dataFile.onload = function() {
+        getQuestion()
+        hamburger.click()
+        progressBar.style.borderColor = "rgb(41, 41, 41)"
+        points = 0
+    }
+    document.getElementsByTagName("head")[0].appendChild(dataFile)
+})
+buttonH3C1.addEventListener("click", function() {
+    let dataFile = document.createElement("script")
+    dataFile.setAttribute("src", "data_H3C1.js")
     dataFile.onload = function() {
         getQuestion()
         hamburger.click()
