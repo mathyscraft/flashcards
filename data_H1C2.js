fbe6cce4e8db0dd1a044c9c6efeaa6f768bc809d
mathys_craft <41139317+mathyscraft@users.noreply.github.com>
2023-03-26 09:49:15 +0200

    add new flashcards

---
+++ data_H1C2.js
@@ -0,0 +1,296 @@
+
+var points=0
+var answered=0
+var question=new Array
+
+// MONARCHIE LIMITEE (1815-1848)
+
+i=1
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qu'accorde Louis XVIII à son retour en 1814 ?"
+question[i].reponse="CHARTE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne sous la Restauration les partisans d'un retour à l'absolutisme ?"
+question[i].reponse="ULTRAS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est le chef de file des ultras sous le règne de Louis XVIII ?"
+question[i].reponse="CHARLES D'ARTOIS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on sous la Restauration ceux qui réclament des progrès pour les libertés et le retour du drapeau tricolore ?"
+question[i].reponse="LIBERAUX"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui succède à Louis XVIII ?"
+question[i].reponse="CHARLES X"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="A quelle cérémonie Charles X participe-t-il le 29 mai 1825 ?"
+question[i].reponse="SACRE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom donne-t-on aux journées des 27, 28 et 29 juillet 1830 ?"
+question[i].reponse="TROIS GLORIEUSES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel roi de France est chassé par la révolution des Trois Glorieuses ?"
+question[i].reponse="CHARLES X"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois et année) se déroule la révolution des Trois Glorieuses ?"
+question[i].reponse="JUILLET 1830"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui devient roi de France au début d'août 1830 ?"
+question[i].reponse="LOUIS-PHILIPPE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel symbole rétablit la Monarchie de Juillet ?"
+question[i].reponse="DRAPEAU TRICOLORE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel est le titre porté par Louis-Philippe ?"
+question[i].reponse="ROI DES FRANCAIS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel jour Louis-Philippe abdique-t-il ?"
+question[i].reponse="24 FEVRIER 1848"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Par quel nom désigne-t-on la période des règnes de Louis XVIII et Charles X en France ?"
+question[i].reponse="RESTAURATION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Par quel nom désigne-t-on la période où Louis-Philippe règne en France ?"
+question[i].reponse="MONARCHIE DE JUILLET"
+question[i].indice="--"
+
+// EUROPE
+
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel roi, revenu en Espagne en 1814, annule la constitution libérale adoptée en 1812 ?"
+question[i].reponse="FERDINAND VII"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Où est signé le traité qui réorganise l'Europe après la défaite de Napoléon Ier ?"
+question[i].reponse="VIENNE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année est signé le traité de Vienne ?"
+question[i].reponse="1815"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel diplomate autrichien est le grand ordonnateur du traité de Vienne ?"
+question[i].reponse="KLEMENS WENZEL VON METTERNICH"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel diplomate représentait la France au congrès de Vienne ?"
+question[i].reponse="CHARLES-MAURICE DE TALLEYRAND-PERIGORD"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on les idées qui affirment que chaque individu possède des droits naturels fondamentaux supérieurs à ceux de tout pouvoir ?"
+question[i].reponse="IDEES LIBERALES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quelle réforme réclament les partisans de systèmes politiques démocratiques ?"
+question[i].reponse="SUFFRAGE UNIVERSEL"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne un groupe humain qui constitue une communauté politique établie sur un territoire défini ?"
+question[i].reponse="NATION"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel mot désigne les peuples qui n'ont pas un territoire national indépendant ?"
+question[i].reponse="NATIONALITES"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Dans quel pays un soulèvement militaire en janvier 1820 conduit-il le roi à accepter la constitution de 1812 ?"
+question[i].reponse="ESPAGNE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui (titre) est assassiné à paris par un républicain en février 1820 ?"
+question[i].reponse="DUC DE BERRY"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Qui est le duc de Berry pour le roi Louis XVIII ? (SON XXX)"
+question[i].reponse="SON NEVEU"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Sur quelle île la population grecque est-elle réduite en esclavage ou massacrée par les Turcs en avril 1822 ?"
+question[i].reponse="CHIOS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Au cours de quelle réunion internationale les grandes puissances soutiennent-elles les Turcs contre les Grecs et décident-elles une intervention militaire en Espagne ?"
+question[i].reponse="CONGRES DE VERONE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="En quelle année les troupes françaises aident-elles Ferdinand VII à rétablir la monarchie absolue en Espagne ?"
+question[i].reponse="1823"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Dans quel pays les troupes françaises interviennent-elles en 1823 ?"
+question[i].reponse="ESPAGNE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel peintre français peint en 1824 les massacres de Chios ?"
+question[i].reponse="EUGENE DELACROIX"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel peuple proclame son indépendance en 1822 et l'obtient en 1829-1830 ?"
+question[i].reponse="GRECS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel pays proclame son indépendance en 1830 ?"
+question[i].reponse="BELGIQUE"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel compositeur exalte dans ses <em>Polonaises</em> le souvenir de sa terre natale ?"
+question[i].reponse="FREDERIC CHOPIN"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="De quel peuple les Polonais écrasent-ils la révolte en 1831 ?"
+question[i].reponse="POLONAIS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel peuple le royaume de Prusse entend-il unifier sous sa domination ?"
+question[i].reponse="ALLEMANDS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quand (mois + année) des étudiants allemands manifestent-ils au château de Wartburg leur volonté de faire l'unité allemande ?"
+question[i].reponse="OCTOBRE 1817"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel peuple le royaume de Piémont-Sardaigne entend-il unifier sous sa domination ?"
+question[i].reponse="ITALIENS"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Quel nom italien portent le smembres de sociétés secrètes luttant contre les décisions imposées par le traité de Vienne ?"
+question[i].reponse="CARBONARI"
+question[i].indice="--"
+
+i++
+question[i]=new Object
+question[i].asked=false
+question[i].texte="Comment appelle-t-on la contestation importante contre le traité de Vienne qui se déclenche en 1848 ?"
+question[i].reponse="PRINTEMPS DES PEUPLES"
+question[i].indice="--"
---
+++ navigation.js
@@ -0,0 +1,34 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
+
+const buttonH1C1 = document.getElementById("H1C1");
+const buttonH1C2 = document.getElementById("H1C2");
+
+hamburger.addEventListener("click", function() {
+    menu.hidden = !menu.hidden
+    overlay.hidden =  ! overlay.hidden
+})
+
+buttonH1C1.addEventListener("click", function() {
+    let dataFile = document.createElement("script")
+    dataFile.setAttribute("src", "data_H1C1.js")
+    dataFile.onload = function() {
+        getQuestion()
+        hamburger.click()
+        progressBar.style.borderColor = "rgb(41, 41, 41)"
+        points = 0
+    }
+    document.getElementsByTagName("head")[0].appendChild(dataFile)
+})
+buttonH1C2.addEventListener("click", function() {
+    let dataFile = document.createElement("script")
+    dataFile.setAttribute("src", "data_H1C2.js")
+    dataFile.onload = function() {
+        getQuestion()
+        hamburger.click()
+        progressBar.style.borderColor = "rgb(41, 41, 41)"
+        points = 0
+    }
+    document.getElementsByTagName("head")[0].appendChild(dataFile)
+})
